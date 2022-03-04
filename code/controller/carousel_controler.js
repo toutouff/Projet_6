893c7e602d1dd9aa0e6ff64cebf1828e62ff400e
toutouff <antonincharpentier00@gmail.com>
2022-03-04 11:32:43 +0100

    ajout du slider slider_width = 1

--- code/controller/carousel_controler.js
+++ code/controller/carousel_controler.js
@@ -1,3 +1,5 @@
+let pos = 0
+
 async function make_slider(category){
   let slider = await gen_slider(category,1)
   slider.gen_card_list()
@@ -11,5 +13,54 @@ async function make_slider(category){
   return await slider_doc
 }
 async function append_slider(parent, category){
-  parent.appendChild(await make_slider(category))
+  let slider = await make_slider(category)
+  slider_setter(await slider)
+  parent.appendChild(await slider)
+}
+
+function slider_setter(slider_doc){
+  let pos = 0
+  let card_list = slider_doc.getElementsByClassName('card_item')
+  update_slide(card_list)
+  let btn_list = slider_doc.getElementsByClassName('carousel_btn')
+  for (let btn of btn_list){
+    switch (btn.innerText) {
+      case '<':
+        btn.onclick = function(){left(card_list)}
+        break;
+      case '>':
+        btn.onclick = function(){right(card_list)}
+        break;
+    }
+  }
+  function left(card_list) {
+    if (pos > 0){
+      pos--
+    }
+    else if (pos == 0){
+      pos = card_list.length - 1
+    }
+    update_slide(card_list)
+  }
+  function right(card_list) {
+    if (pos < card_list.length -1){
+      pos ++
+    }
+    else if (pos == card_list.length-1)
+    {
+      pos = 0
+    }
+    update_slide(card_list)
+  }
+
+  function update_slide(card_list){
+    for(let card of card_list){
+      card.classList.remove('--visible')
+      card.classList.add('--hidden')
+    }
+    let actual_card = card_list[pos]
+    actual_card.classList.remove('--hidden')
+    actual_card.classList.add('--visible')
+  }
+
 }
